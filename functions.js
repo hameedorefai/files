4a06e7f94f3d6621ca251b3511092573f23d54ed
Mahmoud Refai <98220943+hameedorefai@users.noreply.github.com>
2025-12-26 12:46:46 +0200

    Replaced api url to a new one.

--- functions.js
+++ functions.js
@@ -101,7 +101,7 @@ function calculateFinalMark() {
     }
 
     // داخل دالة calculateFinalMark
-    fetch('https://zmyl.azurewebsites.net/api/Log/GetFinalMarkStatsAsyncLast24Hours')
+    fetch('https://zamayl.azurewebsites.net/api/Log/GetFinalMarkStatsAsyncLast24Hours')
         .then(response => response.json())
         .then(data => {
             const currentMark = parseFloat(document.getElementById('finalMarkResult').textContent.replace(/[^\d.]+/g, ''));
@@ -645,7 +645,7 @@ async function fetchDataCourse(text, type) {
         }
     } else {
         try {
-            const response = await fetch('https://zmyl.azurewebsites.net/api/Course/List');
+            const response = await fetch('https://zamayl.azurewebsites.net/api/Course/List');
             if (!response.ok) {
                 if (type == 'submit') {
                     summarySearchBox.innerHTML = `<div class="col-12 text-center fw-bold p-5 text-danger">حدث خطأ ما ، يرجى إعادة تحميل الصفحة</div>`;
@@ -1157,7 +1157,7 @@ async function logPageVisit() {
         message: 'User Has Clicked On The Join Facebook Group Button'
     }
     try {
-        fetch('https://zmyl.azurewebsites.net/api/Log/loginfo', {
+        fetch('https://zamayl.azurewebsites.net/api/Log/loginfo', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -1185,7 +1185,7 @@ async function logPageVisitByFixedButton() {
         message: 'User Has Clicked On The Join Facebook Group Button By Fixed Button'
     }
     try {
-        fetch('https://zmyl.azurewebsites.net/api/Log/loginfo', {
+        fetch('https://zamayl.azurewebsites.net/api/Log/loginfo', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -1379,7 +1379,7 @@ async function handleImageToPdfSubmission(event) {
 
 
 try {
-    const response = await fetch("https://zmyl.azurewebsites.net/api/Course/List");
+    const response = await fetch("https://zamayl.azurewebsites.net/api/Course/List");
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
@@ -1593,7 +1593,7 @@ async function LogSubmitPDFConvertor() {
         message: 'User Has Clicked On PDF Convertor Button'
     }
     try {
-        fetch('https://zmyl.azurewebsites.net/api/Log/loginfo', {
+        fetch('https://zamayl.azurewebsites.net/api/Log/loginfo', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -1620,7 +1620,7 @@ async function LogError(errorText) {
 
     try {
         // انتظار استجابة fetch باستخدام await
-        const response = await fetch('https://zmyl.azurewebsites.net/api/Log/logerror', {
+        const response = await fetch('https://zamayl.azurewebsites.net/api/Log/logerror', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -1656,4 +1656,5 @@ function ReportButton(data) {
     الإبلاغ عن مشكلة
 </button>
     `);
+
         }
